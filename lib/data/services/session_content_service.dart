58462a55886646aace9c5aea80062998ea028897
Kyllian Pinceel <kyllian.pinceel.etu@univ-lille.fr>
2024-11-12 17:01:31 +0100

    ✨ Session Content Service

---
+++ lib/data/services/session_content_service.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:my_fit_buddy/data/models/session_content.dart';
+import 'package:my_fit_buddy/data/services/api_service.dart';
+
+class SessionContentService {
+  static const String sessionContentUrl = "/sessionContent";
+
+  Future<List<SessionContent>> getSessionContents(String id) async {
+    try {
+      final response = await APIService.instance.request(
+        '$sessionContentUrl/$id',
+        DioMethod.get,
+      );
+      if (response.statusCode == 200) {
+        if (response.data is List) {
+          return (response.data as List)
+              .map((sessionContentJson) => SessionContent.fromJson(sessionContentJson))
+              .toList();
+        } else {
+          print('Les données ne sont pas sous forme de liste');
+          return [];
+        }
+      } else {
+        print('Erreur lors de la récupération des sessions : ${response.statusCode}');
+        return [];
+      }
+    } on DioException catch (e) {
+      print('Request failed: ${e.response?.statusCode}, ${e.message}');
+      return [];
+    }
+  }
+}
