57ccf26c1921ed0c4399e2d0e610f26fe814152b
“Billy <billy.charles.etu@univ-lille.fr>
2024-11-12 15:27:03 +0100

    🩹 Fix flutter issues

--- lib/views/pages/live_session_pages/timer_page.dart
+++ lib/views/pages/live_session_pages/timer_page.dart
@@ -21,11 +21,11 @@ class _TimerPageState extends State<TimerPage> {
     return Scaffold(
       appBar: AppBar(
         title: IconButton(
-              icon: Icon(Icons.arrow_back_ios, color: Colors.black, size: 25),
-              onPressed: () => context.goNamed('home'),
-              padding: const EdgeInsets.all(8),
-              highlightColor: Colors.white.withOpacity(0.1),
-            ),
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 25),
+          onPressed: () => context.goNamed('home'),
+          padding: const EdgeInsets.all(8),
+          highlightColor: Colors.white.withOpacity(0.1),
+        ),
       ),
       body: Center(
         child: Column(
@@ -33,79 +33,78 @@ class _TimerPageState extends State<TimerPage> {
             CircularCountDownTimer(
               // Countdown duration in Seconds.
               duration: _duration,
-            
+
               // Countdown initial elapsed Duration in Seconds.
               initialDuration: 0,
-            
+
               // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
               controller: _controller,
-            
+
               // Width of the Countdown Widget.
               width: MediaQuery.of(context).size.width / 1.5,
-            
+
               // Height of the Countdown Widget.
               height: MediaQuery.of(context).size.height / 2,
-            
+
               // Ring Color for Countdown Widget.
               ringColor: fitBlueDark,
-            
+
               // Ring Gradient for Countdown Widget.
               ringGradient: null,
-            
-            
+
               // Filling Color for Countdown Widget.
               fillColor: fitBlueMiddle,
-            
+
               // Filling Gradient for Countdown Widget.
               fillGradient: null,
-            
+
               // Background Color for Countdown Widget.
               backgroundColor: Colors.white,
-            
+
               // Background Gradient for Countdown Widget.
               backgroundGradient: null,
-            
+
               // Border Thickness of the Countdown Ring.
               strokeWidth: 20.0,
-            
+
               // Begin and end contours with a flat edge and no extension.
               strokeCap: StrokeCap.round,
-            
+
               // Text Style for Countdown Text.
               textStyle: const TextStyle(
                 fontSize: 33.0,
                 color: fitBlueMiddle,
                 fontWeight: FontWeight.bold,
               ),
-            
+
               // Format for the Countdown Text.
               textFormat: CountdownTextFormat.MM_SS,
-            
+
               // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
               isReverse: true,
-            
+
               // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
               isReverseAnimation: false,
-            
+
               // Handles visibility of the Countdown Text.
               isTimerTextShown: true,
-            
+
               // Handles the timer start.
               autoStart: true,
-            
+
               // This Callback will execute when the Countdown Starts.
               onStart: () {
                 // Here, do whatever you want
                 debugPrint('Countdown Started');
               },
-            
+
               // This Callback will execute when the Countdown Ends.
               onComplete: () {
                 // Here, do whatever you want
                 context.goNamed('home');
                 debugPrint('Countdown Ended');
               },
-            
+
               // timeFormatterFunction: (defaultFormatterFunction, duration) {
               //   if (duration.inSeconds == 0) {
               //     //only format for '0'
@@ -115,28 +114,28 @@ class _TimerPageState extends State<TimerPage> {
               //     return Function.apply(defaultFormatterFunction, [duration]);
               //   }
               // },
-            
+
               // This Callback will execute when the Countdown Changes.
               onChange: (String timeStamp) {
                 // Here, do whatever you want
                 debugPrint('Countdown Changed $timeStamp');
               },
             ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  _button(
-                    title: "Passer",
-                    onPressed: () => context.goNamed('home'),
-                  ),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                ],
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  width: 30,
+                ),
+                _button(
+                  title: "Passer",
+                  onPressed: () => context.goNamed('home'),
+                ),
+                const SizedBox(
+                  width: 30,
+                ),
+              ],
+            ),
           ],
         ),
       ),
