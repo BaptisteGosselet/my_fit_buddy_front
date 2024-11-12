7146c43d7634882dc7ffb4fd7a43d0fdd28326bb
“Billy <billy.charles.etu@univ-lille.fr>
2024-11-12 15:34:27 +0100

    💡 Delete comment

--- lib/views/pages/live_session_pages/timer_page.dart
+++ lib/views/pages/live_session_pages/timer_page.dart
@@ -31,95 +31,34 @@ class _TimerPageState extends State<TimerPage> {
         child: Column(
           children: [
             CircularCountDownTimer(
-              // Countdown duration in Seconds.
               duration: _duration,
-
-              // Countdown initial elapsed Duration in Seconds.
               initialDuration: 0,
-
-              // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
               controller: _controller,
-
-              // Width of the Countdown Widget.
               width: MediaQuery.of(context).size.width / 1.5,
-
-              // Height of the Countdown Widget.
               height: MediaQuery.of(context).size.height / 2,
-
-              // Ring Color for Countdown Widget.
               ringColor: fitBlueDark,
-
-              // Ring Gradient for Countdown Widget.
               ringGradient: null,
-
-              // Filling Color for Countdown Widget.
               fillColor: fitBlueMiddle,
-
-              // Filling Gradient for Countdown Widget.
               fillGradient: null,
-
-              // Background Color for Countdown Widget.
               backgroundColor: Colors.white,
-
-              // Background Gradient for Countdown Widget.
               backgroundGradient: null,
-
-              // Border Thickness of the Countdown Ring.
               strokeWidth: 20.0,
-
-              // Begin and end contours with a flat edge and no extension.
               strokeCap: StrokeCap.round,
-
-              // Text Style for Countdown Text.
               textStyle: const TextStyle(
                 fontSize: 33.0,
                 color: fitBlueMiddle,
                 fontWeight: FontWeight.bold,
               ),
-
-              // Format for the Countdown Text.
               textFormat: CountdownTextFormat.MM_SS,
-
-              // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
               isReverse: true,
-
-              // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
               isReverseAnimation: false,
-
-              // Handles visibility of the Countdown Text.
               isTimerTextShown: true,
-
-              // Handles the timer start.
               autoStart: true,
-
-              // This Callback will execute when the Countdown Starts.
-              onStart: () {
-                // Here, do whatever you want
-                debugPrint('Countdown Started');
-              },
-
-              // This Callback will execute when the Countdown Ends.
+              onStart: () {},
               onComplete: () {
-                // Here, do whatever you want
                 context.goNamed('home');
-                debugPrint('Countdown Ended');
               },
-
-              // timeFormatterFunction: (defaultFormatterFunction, duration) {
-              //   if (duration.inSeconds == 0) {
-              //     //only format for '0'
-              //     return "Start";
-              //   } else {
-              //     //others durations by it's default format
-              //     return Function.apply(defaultFormatterFunction, [duration]);
-              //   }
-              // },
-
-              // This Callback will execute when the Countdown Changes.
-              onChange: (String timeStamp) {
-                // Here, do whatever you want
-                debugPrint('Countdown Changed $timeStamp');
-              },
+              onChange: (String timeStamp) {},
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
