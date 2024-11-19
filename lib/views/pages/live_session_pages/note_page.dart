52fa8f058386e0f56da57f049f094641c47af219
“Billy <billy.charles.etu@univ-lille.fr>
2024-11-19 16:01:10 +0100

    🎨 Fix format

--- lib/views/pages/live_session_pages/note_page.dart
+++ lib/views/pages/live_session_pages/note_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_fit_buddy/views/themes/color.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:my_fit_buddy/views/widgets/exercice_card_scroll.dart';
 
 const List<String> list = <String>['🤷‍♂️', '😐', '👍', '😎'];
 
@@ -16,15 +15,14 @@ class NotePage extends StatefulWidget {
   State<NotePage> createState() => _NotePageState();
 }
 
-class _NotePageState extends State<NotePage> with SingleTickerProviderStateMixin {
-
+class _NotePageState extends State<NotePage>
+    with SingleTickerProviderStateMixin {
   late final CustomTimerController _controller = CustomTimerController(
-    vsync: this,
-    begin: const Duration(minutes: 3),
-    end: const Duration(),
-    initialState: CustomTimerState.reset,
-    interval: CustomTimerInterval.milliseconds
-  );
+      vsync: this,
+      begin: const Duration(minutes: 3),
+      end: const Duration(),
+      initialState: CustomTimerState.reset,
+      interval: CustomTimerInterval.milliseconds);
 
   String dropdownValue = list.first;
 
@@ -32,101 +30,102 @@ class _NotePageState extends State<NotePage> with SingleTickerProviderStateMixin
   Widget build(BuildContext context) {
     _controller.start();
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: IconButton(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: IconButton(
           icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 25),
           onPressed: () => context.goNamed('home'),
           padding: const EdgeInsets.all(8),
           highlightColor: Colors.white.withOpacity(0.1),
         ),
-        ),
-        body: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CustomTimer(
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomTimer(
                   controller: _controller,
                   builder: (state, time) {
                     // Build the widget you want!🎉
-                    return Text(
-                      "${time.minutes}:${time.seconds}",
-                      style: const TextStyle(fontSize: 70.0, color: fitBlueMiddle)
-                    );
-                  }
+                    return Text("${time.minutes}:${time.seconds}",
+                        style: const TextStyle(
+                            fontSize: 70.0, color: fitBlueMiddle));
+                  }),
+            ],
+          ),
+          Expanded(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 40.0, vertical: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text("NOTES"),
+                  TextField(
+                    maxLines: 12,
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      hintText:
+                          AppLocalizations.of(context)!.textNotePlaceholder,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                DropdownMenu<String>(
+                  textAlign: TextAlign.center,
+                  initialSelection: list.first,
+                  onSelected: (String? value) {
+                    // This is called when the user selects an item.
+                    setState(() {
+                      dropdownValue = value!;
+                    });
+                  },
+                  dropdownMenuEntries:
+                      list.map<DropdownMenuEntry<String>>((String value) {
+                    return DropdownMenuEntry<String>(
+                        value: value, label: value);
+                  }).toList(),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    backgroundColor: fitBlueDark,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    elevation: 4,
+                    shadowColor: Colors.grey.withOpacity(0.5),
+                  ),
+                  onPressed: () {
+                    context.goNamed('home'); //TODO
+                  },
+                  child: SizedBox(
+                    width: 130,
+                    height: 60,
+                    child: Center(
+                      child: Text(
+                        AppLocalizations.of(context)!.validateButton,
+                        style: const TextStyle(
+                          color: fitCloudWhite,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const Text("NOTES"),
-                    TextField(
-                      maxLines: 12,
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        hintText: AppLocalizations.of(context)!.textNotePlaceholder,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-              Padding(
-                padding: const EdgeInsets.all(40.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    DropdownMenu<String>(
-                      textAlign: TextAlign.center,
-                      initialSelection: list.first,
-                      onSelected: (String? value) {
-                        // This is called when the user selects an item.
-                        setState(() {
-                          dropdownValue = value!;
-                        });
-                      },
-                      dropdownMenuEntries: list.map<DropdownMenuEntry<String>>((String value) {
-                        return DropdownMenuEntry<String>(value: value, label: value);
-                      }).toList(),
-                    ),
-                    TextButton(
-                      style: TextButton.styleFrom(
-                        padding: EdgeInsets.zero,
-                        backgroundColor: fitBlueDark,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        elevation: 4,
-                        shadowColor: Colors.grey.withOpacity(0.5),
-                        
-                      ),
-                      onPressed: () {
-                        context.goNamed('home'); //TODO
-                      },
-                      child: SizedBox(
-                        width: 130,
-                        height: 60,
-                        child: Center(
-                          child: Text(
-                            AppLocalizations.of(context)!.validateButton,
-                            style: const TextStyle(
-                              color: fitCloudWhite,
-                              fontSize: 14,
-                            ),
-                          ),
-                        ),
-                      ),
-                  ),
-                  ],
-                ),
-              ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
