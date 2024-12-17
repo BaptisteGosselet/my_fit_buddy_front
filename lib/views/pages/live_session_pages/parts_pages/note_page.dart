205ee3db828bdb42db96576b09ceea26a1aa724d
Baptiste Gosselet <baptiste.gosselet@outlook.fr>
2024-12-17 10:09:57 +0100

    :bug: - bande note

--- lib/views/pages/live_session_pages/parts_pages/note_page.dart
+++ lib/views/pages/live_session_pages/parts_pages/note_page.dart
@@ -35,7 +35,7 @@ class _NotePageState extends State<NotePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: IconButton(
           icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 25),
@@ -44,10 +44,10 @@ class _NotePageState extends State<NotePage> {
           highlightColor: Colors.white.withOpacity(0.1),
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Padding(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 40.0, vertical: 10),
               child: Column(
@@ -57,6 +57,7 @@ class _NotePageState extends State<NotePage> {
                   TextField(
                     controller: _textController,
                     maxLines: 12,
+                    maxLength: 255,
                     decoration: InputDecoration(
                       border: const OutlineInputBorder(),
                       hintText:
@@ -66,65 +67,65 @@ class _NotePageState extends State<NotePage> {
                 ],
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(40.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                DropdownMenu<String>(
-                  textAlign: TextAlign.center,
-                  initialSelection: emojiRatingMap.keys.first,
-                  onSelected: (String? value) {
-                    setState(() {
-                      dropdownValue = value!;
-                    });
-                  },
-                  dropdownMenuEntries: emojiRatingMap.keys
-                      .map<DropdownMenuEntry<String>>((String key) {
-                    return DropdownMenuEntry<String>(value: key, label: key);
-                  }).toList(),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.zero,
-                    backgroundColor: fitBlueDark,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    elevation: 4,
-                    shadowColor: Colors.grey.withOpacity(0.5),
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  DropdownMenu<String>(
+                    textAlign: TextAlign.center,
+                    initialSelection: emojiRatingMap.keys.first,
+                    onSelected: (String? value) {
+                      setState(() {
+                        dropdownValue = value!;
+                      });
+                    },
+                    dropdownMenuEntries: emojiRatingMap.keys
+                        .map<DropdownMenuEntry<String>>((String key) {
+                      return DropdownMenuEntry<String>(value: key, label: key);
+                    }).toList(),
                   ),
-                  onPressed: () async {
-                    final text = _textController.text;
-                    final rate = getSelectedRate();
-                    final success =
-                        await widget.onValidate(text, rate, context);
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      backgroundColor: fitBlueDark,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      elevation: 4,
+                      shadowColor: Colors.grey.withOpacity(0.5),
+                    ),
+                    onPressed: () async {
+                      final text = _textController.text;
+                      final rate = getSelectedRate();
+                      final success =
+                          await widget.onValidate(text, rate, context);
 
-                    if (mounted && success) {
-                      if (context.mounted) {
-                        context.goNamed('home');
+                      if (mounted && success) {
+                        if (context.mounted) {
+                          context.goNamed('home');
+                        }
                       }
-                    }
-                  },
-                  child: SizedBox(
-                    width: 130,
-                    height: 60,
-                    child: Center(
-                      child: Text(
-                        AppLocalizations.of(context)!.validateButton,
-                        style: const TextStyle(
-                          color: fitCloudWhite,
-                          fontSize: 14,
+                    },
+                    child: SizedBox(
+                      width: 130,
+                      height: 60,
+                      child: Center(
+                        child: Text(
+                          AppLocalizations.of(context)!.validateButton,
+                          style: const TextStyle(
+                            color: fitCloudWhite,
+                            fontSize: 14,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
